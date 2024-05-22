17b85428be6d12fd63e53f66d212fb63a65608fb
kahuho <kahuhobrian7@gmail.com>
2024-05-22 19:21:41 +0300

    Minor db changes

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'kahuhobrian0@gmail.com',
+            'password' => Hash::make('Brykahn#123'), // change this to a secure password
+            'is_admin' => true, // Assuming you have an `is_admin` field to distinguish admin users
+        ]);
+    }
+}
